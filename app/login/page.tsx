bc5b76302200422edfb210e12dfb6d0e5b84019b
daichi-iwamoto <tay1fai0y@gmail.com>
2023-07-03 19:53:43 +0900

    feat: add login function
    close #2

---
+++ app/login/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '@/src/aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
+  return (
+    <Authenticator hideSignUp>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
