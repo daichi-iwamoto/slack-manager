f0463659a973a8f060324670b17ea24a0b68fc92
daichi-iwamoto <tay1fai0y@gmail.com>
2023-07-04 00:30:00 +0900

    feat: add env setting

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'avatars.slack-edge.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.slack-edge.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  env: {
+    SLACK_TOKEN: process.env.SLACK_TOKEN,
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
